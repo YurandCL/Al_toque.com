38824a343861b24053430ab5843f598b42d9e876
Yurand Jorge Coloma Linares <yurand.coloma@tecsup.edu.pe>
2018-06-19 15:40:15 -0500

    implementacion de slide en texto informativo de cada imagen

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 	});
 //---------------------------------------------------------------
 
-//uso del slide
+//uso del slide en imagenes
 	var slideIndex = 0;
 	showSlides();
 	function showSlides(){
@@ -61,5 +61,21 @@ $(document).ready(function(){
 		}
 		slides[slideIndex-1].style.display = "block";
 		setTimeout(showSlides,2000);
+	}
+	//uso de slides en su descripcion.
+	var slideIndex2 = 0;
+	showSlides2();
+	function showSlides2(){
+		var j;
+		var slides2=document.getElementsByClassName("mySlides2");
+		for(j=0; j<slides2.length; j++){
+			slides2[j].style.display = "none";
+		}
+		slideIndex2++;
+		if(slideIndex2 > slides2.length){
+			slideIndex2 = 1;
+		}
+		slides2[slideIndex2-1].style.display = "block";
+		setTimeout(showSlides2,4000);
 	}
 });
