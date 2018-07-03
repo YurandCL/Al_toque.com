63ae405ac9a34a5d0f01cad2d8b82e0611bb9180
BHuanca <Bladimir.huanca@tecsup.edu.pe>
2018-07-03 17:46:39 -0500

    ignoren esto

--- rails/altoq/Altoque/config/routes.rb
+++ rails/altoq/Altoque/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :altoqs do
 	collection do
-          get :login
+
+          get :registro
 	end
   end
   get 'altoqs/index'
 
-  root 'altoqs#index' 
+  root 'altoqs#login'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
