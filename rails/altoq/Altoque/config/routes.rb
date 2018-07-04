64771685684e008a2c946142ff300d6a1b936f26
BHuanca <Bladimir.huanca@tecsup.edu.pe>
2018-07-04 14:01:20 -0500

    Se mejoro el login

--- rails/altoq/Altoque/config/routes.rb
+++ rails/altoq/Altoque/config/routes.rb
@@ -7,10 +7,5 @@ Rails.application.routes.draw do
   end
   get 'altoqs/index'
 
-<<<<<<< HEAD
-  root 'altoqs#login' 
-=======
   root 'altoqs#login'
->>>>>>> 179cc583be6ce181aa21ebd69a50ecd88ad0cfed
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
